bd7074f409df932515e3ae5fa1d95e7e97ca256a
Ira Tka4eva <ira.tka4eva2011@yandex.ru>
2024-12-14 19:52:03 +0300

    Export and import completed

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const game = new Game();
+game.start();
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js'
---
+++ src/game.js
@@ -0,0 +1,20 @@
+import Character from './domain';
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  export default Game;
+
+  export { GameSavingData, readGameSaving, writeGameSaving };
